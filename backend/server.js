cc4fa9a323c7834bd8bcd7c7b8bac98567c0c9d2
NetAcad DEVASC <changeme@example.com>
2025-12-15 15:37:09 +0000

    fix port render

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.post("/api/messages", (req, res) => {
   res.status(201).json({ message: "Message ajouté" });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
